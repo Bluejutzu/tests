d5ed258dcf1d9c0003f50f59768d3198d9b4a094
bluejutzu <kalebdaniel98@gmail.com>
2024-11-17 15:32:55 +0100

    change

--- src/package/utils.ts
+++ src/package/utils.ts
@@ -1,19 +1,25 @@
-export const validateRegex = (inputString: string, regex: RegExp, test?: boolean): boolean => {
+const validateRegex = (inputString: string, regex: RegExp, test?: boolean): boolean => {
     const result = regex.exec(inputString);
     if (!result || typeof result == null) {
         return false;
     } else {
-        test ? console.log(result[0]) : null;
+        test ? console.log('Match found:', result[0]) : null;
         return true;
     }
 };
 
-export const getDate = () => {
+const getDate = () => {
     const now = new Date();
     const dateString = `${now.toISOString().slice(0, 10)}_${now.toTimeString().slice(0, 5).replace(":", "-")}`;
     return dateString;
 };
 
-export const delay = async (ms: number) => {
+const delay = async (ms: number) => {
     return await new Promise(resolve => resolve(ms));
 };
+
+export const utils = {
+    validateRegex,
+    getDate,
+    delay
+}
